eaf073ea5ece989a61665d7ef6c340ce4b41f1b5
arnold-aswan <fredyaswan@gmail.com>
2024-01-17 14:15:52 +0300

    created account page

---
+++ src/pages/Account.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import SavedShows from "../components/SavedShows";
+
+const Account = () => {
+  return (
+    <>
+      <div className="w-full ">
+        <img
+          className="w-full h-[400px] object-cover"
+          src="https://wpassets.brainstation.io/app/uploads/2017/04/13100509/Netflix-Background.jpg"
+          alt="background image"
+        />
+        <div className="bg-black/60 fixed top-0 left-0 w-full h-[550px] "></div>
+        <div className="absolute top-[20%] p-4 md:p-8 ">
+          <h1 className="text-3xl md:text-5xl font-bold">My Shows</h1>
+        </div>
+      </div>
+      <SavedShows />
+    </>
+  );
+};
+
+export default Account;
