e4cc447064748baea3d55de24da9418fc82cc0ac
arnold-aswan <fredyaswan@gmail.com>
2024-01-31 22:22:01 +0300

    removed save function from movie component

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,37 +1,9 @@
 import React, { useState } from "react";
-import { FaHeart, FaRegHeart } from "react-icons/fa";
-import { UserAuth } from "../context/AuthContext";
-import { db } from "../firebase";
-import { arrayUnion, doc, updateDoc } from "firebase/firestore";
 import BasicModal from "./Modal";
 import defaultImg from "../public/assets/thumbnail.jpg";
 
 const Movie = ({ movie }) => {
-  const [like, setLike] = useState(false);
-  const [saved, setSaved] = useState(false);
-  const { user } = UserAuth();
-
   const [open, setOpen] = useState(false);
-
-
-
-  const movieId = doc(db, "users", `${user?.email}`);
-
-  const saveShow = async () => {
-    if (user?.email) {
-      setLike(!like);
-      setSaved(true);
-      await updateDoc(movieId, {
-        savedShows: arrayUnion({
-          id: movie.id,
-          title: movie.title,
-          img: movie.backdrop_path,
-        }),
-      });
-    } else {
-      alert("Login to save movies");
-    }
-  };
 
   return (
     <>
@@ -46,15 +18,6 @@ const Movie = ({ movie }) => {
         <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white  ">
           <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center ">
             {movie?.title}
-          </p>
-
-          <p onClick={saveShow}>
-            {" "}
-            {like ? (
-              <FaHeart className="absolute left-4 top-4 text-gray-300" />
-            ) : (
-              <FaRegHeart className="absolute top-4 left-4 text-gray-300" />
-            )}{" "}
           </p>
         </div>
       </div>
